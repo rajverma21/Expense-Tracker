9c82441521697e4de1943c47abc28daf624d6968
rajverma21 <119425794+rajverma21@users.noreply.github.com>
2024-06-01 22:13:45 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const app = express()
 
 app.use(
   cors({
-    origin: ['https://expense-tracker-frontend-pied.vercel.app'],
+    origin: ['https://mernexpensetracker.vercel.app'],
     methods: ['POST', 'GET', 'PUT', 'DELETE'],
     credentials: true
   })
