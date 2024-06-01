15cb6de84eae8e0040fac91637ce63425b401f89
rajverma21 <119425794+rajverma21@users.noreply.github.com>
2024-06-01 21:17:13 +0530

    vercel deployment check

--- backend/index.js
+++ backend/index.js
@@ -6,11 +6,17 @@ import router from './routes/expenseRoutes.js'
 
 dotenv.config()
 const app = express()
-app.use(express.static('dist'))
+// app.use(express.static('dist'))
 
 // MIDDLEWARES
 
-app.use(cors())
+app.use(
+  cors({
+    origin: ['https://deploy-mern-1whq.vercel.app'],
+    methods: ['POST', 'GET'],
+    credentials: true
+  })
+)
 app.use(express.json())
 app.use('/expenses', router)
 
