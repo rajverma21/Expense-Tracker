0a604fc4b3d5d8d506f86ba6814c146105bf82c2
rajverma21 <119425794+rajverma21@users.noreply.github.com>
2024-06-01 21:21:58 +0530

    Update requestMethods.jsx

--- frontend/src/requestMethods.jsx
+++ frontend/src/requestMethods.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const BASE_URL = 'http://localhost:5000'
+const BASE_URL = 'https://expense-tracker-pi-mauve.vercel.app'
 
 const publicRequest = axios.create({
   baseURL: BASE_URL
